13a5fc18b1fd9da7091743788f46c3e42aec9059
cotore-game <cottonstar.watahoshi@gmail.com>
2026-01-07 16:25:38 +0900

    feat: 素因数分解 計算量を考えない実装 あと改行追加

--- main.c
+++ main.c
@@ -4,7 +4,24 @@ int main(){
     int input;
     scanf("%d", &input);
 
-    printf("%d\n", input);
+    int devide = 2;
+    while(1){
+        if(input == 1 || input < 1) break;
+
+        while(input % devide == 0){
+            input /= devide;
+            printf("%d", devide);
+
+            if(input == 1){
+                printf("\n");
+                break;
+            }else{
+                printf(" * ");
+            }
+        }
+
+        devide++;
+    }
 
     return 0;
 }
